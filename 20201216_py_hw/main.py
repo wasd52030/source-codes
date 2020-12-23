9ea41e2a16608b6afda78c69f051be826555559d
a <b>
2020-12-23 16:58:47 +0800

    update files

--- 20201216_py_hw/main.py
+++ 20201216_py_hw/main.py
@@ -15,6 +15,7 @@ def game_input():
     user_input.clear()
     if rein!=1:
         money-=10
+    rein=0
     doubleyes=0
     justyesvalue=0
     print("你有%d塊"%money)
@@ -37,12 +38,13 @@ def game_input():
 def calc():
     global wincounter,money
     if wincounter<=5:
+        money+=10
         money+=5
     elif wincounter>5:
         money-=5
 
 def run_game():
-    global data,user_input,money,yesvalue,yeslocate,doubleyes,justyesvalue,wincounter
+    global data,user_input,money,yesvalue,yeslocate,doubleyes,justyesvalue,wincounter,rein
     rein=0
     for i in range(0,len(user_input)):
         for x in data:
@@ -63,22 +65,25 @@ def run_game():
     print("皆對%d個，位置錯%d個,猜了%d次"%(doubleyes,justyesvalue,wincounter))
     if doubleyes == 4:
         print("win")
-        user_input.clear()
         calc()
+        main()
     else:
         game_input()
+        rein=0
         
 
-while True:
-    print("現有金額 : %d"%money)
-    print("輸入1進遊戲，輸入0離開:",end="")
-    kin_flag=int(input())
-    if kin_flag==1:
-        x=[0,1,2,3,4,5,6,7,8,9]
-        data=random.sample(x,4)
-        print(data)
-        game_input()
-        if len(user_input)==4:
-            run_game()
-    elif kin_flag==0:
-         break
+def main():
+    global data
+    while True:
+        print("現有金額 : %d"%money)
+        print("輸入1進遊戲，輸入0離開:",end="")
+        kin_flag=int(input())
+        if kin_flag==1:
+            x=[0,1,2,3,4,5,6,7,8,9]
+            data=random.sample(x,4)
+            print(data)
+            game_input()
+        elif kin_flag==0:
+            break
+
+main()

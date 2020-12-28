c88efcc162e73cd92af997824779156b218554b3
a <b>
2020-12-29 00:28:29 +0800

    把split函式獨立成split.h

--- 20201223_cpp_hw/solve1/solve1.cpp
+++ 20201223_cpp_hw/solve1/solve1.cpp
@@ -3,27 +3,9 @@
 #include <vector>
 #include <algorithm>
 #include <regex>
+#include "split.h"
 using namespace std;
 void a(); void b(); void menu();
-
-/*
-  string::find_first_of(const string& str,size_t pos=0)
-  -> 從pos(default->0)開始，尋找第一個和str相符的字串所在的位置，找不到則返回string::npos
-*/
-void split(const string& str, vector<string>& word, const string& key)
-{
-	int start = str.find_first_not_of(key);
-	int end = 0;
-	while (start != string::npos)
-	{
-		end = str.find_first_of(key, start + 1);
-		if (end == string::npos)
-			end = str.length();
-		word.push_back(str.substr(start, end - start));
-		start = str.find_first_not_of(key, end + 1);
-	}
-}
-
 struct Date
 {
 	string year = "";

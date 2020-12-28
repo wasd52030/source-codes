40420e7e349e2bdcb1ddbee303975fe7b49ada14
a <b>
2020-12-28 22:55:35 +0800

    把split函式以簡單直覺的方式改寫

--- 20201223_cpp_hw/solve1/solve1.cpp
+++ 20201223_cpp_hw/solve1/solve1.cpp
@@ -2,18 +2,24 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <sstream>  //string stream
 #include <regex>
 using namespace std;
 
-void split(const std::string& s, std::vector<std::string>& sv, const char delim = ' ')
+/*
+  string::find_first_of(const string& str,size_t pos=0)
+  -> 從pos(default->0)開始，尋找第一個和str相符的字串所在的位置，找不到則返回string::npos
+*/
+void split(const string& str, vector<string>& word, const string& key)
 {
-	sv.clear();
-	std::istringstream iss(s); 
-	std::string temp;
-	while (std::getline(iss, temp, delim))
+	int start = str.find_first_not_of(key);
+	int end = 0;
+	while (start != string::npos)
 	{
-		sv.push_back(temp);
+		end = str.find_first_of(key, start + 1);
+		if (end == string::npos)
+			end = str.length();
+		word.push_back(str.substr(start, end - start));
+		start = str.find_first_not_of(key, end + 1);
 	}
 }
 
@@ -111,7 +117,7 @@ void a()  //選單1
 		cin >> x;
 		if (regex_match(x, input_check))
 		{
-			split(x, split_contianer, '/');
+			split(x, split_contianer, "/");
 			d.year = split_contianer[0];
 			d.month = split_contianer[1];
 			d.date = atoi(split_contianer[2].c_str());

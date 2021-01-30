969ca20db1c2e95e2337b8c7e63f7bdeeb438377
rwxrwx777 <C109152304@nkust.edu.tw>
2021-01-31 01:53:11 +0800

    把以前比賽用的工具試著用不同的方式重寫

---
+++ 7seg_Encoder/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 14F);
+            this.button1.Location = new System.Drawing.Point(69, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "a";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("新細明體", 14F);
+            this.button2.Location = new System.Drawing.Point(191, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(37, 101);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "b";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("新細明體", 14F);
+            this.button3.Location = new System.Drawing.Point(191, 189);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(37, 101);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "c";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("新細明體", 14F);
+            this.button4.Location = new System.Drawing.Point(69, 286);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(116, 31);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "d";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("新細明體", 14F);
+            this.button5.Location = new System.Drawing.Point(26, 189);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(37, 101);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "e";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("新細明體", 14F);
+            this.button6.Location = new System.Drawing.Point(26, 57);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(37, 101);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "f";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("新細明體", 14F);
+            this.button7.Location = new System.Drawing.Point(69, 158);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(116, 31);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "g";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("新細明體", 14F);
+            this.button8.Location = new System.Drawing.Point(208, 296);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(35, 35);
+            this.button8.TabIndex = 7;
+            this.button8.Text = ".";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("新細明體", 15F);
+            this.button9.Location = new System.Drawing.Point(26, 343);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(202, 40);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "編碼";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("新細明體", 24F);
+            this.label1.Location = new System.Drawing.Point(26, 410);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 40);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "0x";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 475);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "7seg encoder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 7seg_Encoder/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Drawing;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        bool[] segstate = new bool[8];
+        int[] segdata = new int[8];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            segstate[0]= !segstate[0];
+            if (segstate[0])
+            {
+                button1.BackColor = Color.Red;
+                segdata[0] = 0x01;
+            }
+            else
+            {
+                button1.BackColor = SystemColors.ControlLight;
+                segdata[0] = 0;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < segstate.Length; i++)
+                segstate[i] = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            segstate[1]= !segstate[1];
+            if (segstate[1])
+            {
+                button2.BackColor = Color.Red;
+                segdata[1] = 0x02;
+            }
+            else
+            {
+                button2.BackColor = SystemColors.ControlLight;
+                segdata[1] = 0;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            segstate[2] = !segstate[2];
+            if (segstate[2])
+            {
+                button3.BackColor = Color.Red;
+                segdata[2] = 0x04;
+            }
+            else
+            {
+                button3.BackColor = SystemColors.ControlLight;
+                segdata[2] = 0;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            segstate[3] = !segstate[3];
+            if (segstate[3])
+            {
+                button4.BackColor = Color.Red;
+                segdata[3] = 0x08;
+            }
+            else
+            {
+                button4.BackColor = SystemColors.ControlLight;
+                segdata[3] = 0;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            segstate[4] = !segstate[4];
+            if (segstate[4])
+            {
+                button5.BackColor = Color.Red;
+                segdata[4] = 0x010;
+            }
+            else
+            {
+                button5.BackColor = SystemColors.ControlLight;
+                segdata[4] = 0;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            segstate[5] = !segstate[5];
+            if (segstate[5])
+            {
+                button6.BackColor = Color.Red;
+                segdata[5] = 0x20;
+            }
+            else
+            {
+                button6.BackColor = SystemColors.ControlLight;
+                segdata[5] = 0;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            segstate[6] = !segstate[6];
+            if (segstate[6])
+            {
+                button7.BackColor = Color.Red;
+                segdata[6] = 0x40;
+            }
+            else
+            {
+                button7.BackColor = SystemColors.ControlLight;
+                segdata[6] = 0;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            segstate[7] = !segstate[7];
+            if (segstate[7])
+            {
+                button8.BackColor = Color.Red;
+                segdata[7] = 0x80;
+            }
+            else
+            {
+                button8.BackColor = SystemColors.ControlLight;
+                segdata[7] = 0;
+            }
+        }
+
+        string dec_to_hex(int i,string o="")
+        {
+            if(i==0)
+            {
+                string r=Strings.StrReverse(o);
+                o = "";
+                return r;
+            }
+            else
+            {
+                string x = (i % 16).ToString();
+                switch(x)
+                {
+                    case "10":
+                        x = "A";
+                        break;
+                    case "11":
+                        x = "B";
+                        break;
+                    case "12":
+                        x = "C";
+                        break;
+                    case "13":
+                        x = "D";
+                        break;
+                    case "14":
+                        x = "E";
+                        break;
+                    case "15":
+                        x = "F";
+                        break;
+                }
+                o += x;
+                return dec_to_hex(i / 16,o);
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            int x = 0;
+            foreach(var i in segdata)
+            {
+                x |= i;
+            }
+            label1.Text = "0x" + dec_to_hex(x);
+        }
+    }
+}

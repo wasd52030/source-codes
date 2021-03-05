8ffbf6337aa99d91f6a7334e82a91c8b5f683485
rwxrwx777 <C109152304@nkust.edu.tw>
2021-03-06 00:23:35 +0800

    no message

--- 20210303_py_hw/main.py
+++ 20210303_py_hw/main.py
@@ -1,14 +1,14 @@
 from tkinter import *
 import math
+
 root=Tk()
 root.title("BSK TRA")
-
 in_data=[]
 num_data=[]
+
 with open('input.txt') as f1:
     for k in f1:
         in_data.append(k)
-
 
 p=0
 for i in range(0,int(in_data[0])+1):
@@ -26,12 +26,12 @@ for i in range(int(in_data[0])+1):
     for k in range(int(in_data[0])+1,i+1,-1):
         out+=" "
     for j in range(0,i+1):
-        out+="  {}  ".format(num_data[x])
+        out+=" {} ".format(num_data[x])
         #out+="  %d  "%(num_data[x])
         x+=1
     out+="  "
     out+="\n"
 
-o=Label(root,text=out).pack()
+o=Label(root,text=out,font=('Arial',16)).pack()
 
 root.mainloop()

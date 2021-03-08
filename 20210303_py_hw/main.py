d9b9fb341fa58000fa3651805413fcf2cfcc6a8c
rwxrwx777 <C109152304@nkust.edu.tw>
2021-03-08 21:16:48 +0800

    no message

--- 20210303_py_hw/main.py
+++ 20210303_py_hw/main.py
@@ -2,32 +2,20 @@ from tkinter import *
 import math
 
 root=Tk()
-root.title("BSK TRA")
+root.title("BSKTRA")
 in_data=[]
 num_data=[]
 
 with open('input.txt') as f1:
-    for k in f1:
-        in_data.append(k)
-
-p=0
-for i in range(0,int(in_data[0])+1):
-    for j in range(0,int(in_data[0])+1):
-        if i==0 and j==1:
-            continue
-        else:
-            p=math.comb(i,j)   #計算nCr(i,j)，python3.8以上始可使用
-            if p>0:
-                num_data.append(p)
+    in_data=[k for k in f1]
 
 x=0
 out=""
-for i in range(int(in_data[0])+1):
+for i in range(1,int(in_data[0])+2):
     for k in range(int(in_data[0])+1,i+1,-1):
         out+=" "
-    for j in range(0,i+1):
-        out+=" {} ".format(num_data[x])
-        #out+="  %d  "%(num_data[x])
+    for j in range(1,i+1):
+        out+=" {} ".format(math.comb(i-1,j-1))
         x+=1
     out+="  "
     out+="\n"

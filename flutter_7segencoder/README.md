a67fe19d615300fda486f2dc97207c14fac6657c
rwxrwx777 <C109152304@nkust.edu.tw>
2021-03-18 21:24:02 +0800

    no message

--- flutter_7segencoder/README.md
+++ flutter_7segencoder/README.md
@@ -1,6 +1,6 @@
 # flutter_7segencoder
 
-A new Flutter application.
+A new Flutter project.
 
 ## Getting Started
 
---
+++ flutter_7segencoder/android/app/src/main/kotlin/com/example/flutter_7segencoder/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_7segencoder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}

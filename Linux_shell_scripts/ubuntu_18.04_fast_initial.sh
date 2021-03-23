1cbeadb3e27a2968afb12923590183db308cb527
rwxrwx777 <C109152304@nkust.edu.tw>
2021-03-23 12:01:38 +0800

    no message

--- Linux_shell_scripts/ubuntu_18.04_fast_initial.sh
+++ Linux_shell_scripts/ubuntu_18.04_fast_initial.sh
@@ -1,6 +1,13 @@
 #建議以家目錄以外的目錄執行
 
-#把非英文的家目錄資料夾更名成英文的
+if [ -f "/$BASH_SOURCE" ]; then
+	echo -e "\nok\n"
+else
+	sudo mv $BASH_SOURCE /
+	echo -e "\n已移動到根目錄\n請從根目錄執行以獲得最佳效果\n"
+    exit 0
+fi
+
 export LANG=en_us
 xdg-user-dirs-gtk-update
 
@@ -12,32 +19,46 @@ echo "alias apt-init=\"sudo apt update && sudo apt upgrade\"" >> ~/.bashrc
 source ~/.bashrc
 
 #裝vim和open-vm-tools
+echo -e "\n開始安裝vim和open-vm-tools\n"
 sudo apt update && sudo apt upgrade
 sudo apt install -y open-vm-tools
 sudo apt install -y vim
 
 #裝python3 tkinter和python3 pip
+echo -e "\n開始安裝python3 tkinter和python3 pip\n"
 sudo apt install -y python3-tk
 sudo apt install -y python3-pip
 
 #裝wget和相關套件(如果沒有的話)
+echo -e "\n開始安裝wget和相關套件\n"
 sudo apt install -y software-properties-common apt-transport-https wget
 
 #裝VScode
+echo -e "\n開始安裝VScode\n"
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update
 sudo apt install -y code
 
 #裝chrome
+echo -e "\n開始安裝chrome\n"
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y ./google-chrome-stable_current_amd64.deb
 
 #移除firefox
+echo -e "\n開始移除firefox\n"
 sudo apt -y remove firefox
 
 #移除chrome安裝檔
+echo -e "\n把chrome安裝檔移除\n"
 sudo rm -rf ./google-chrome-stable_current_amd64.deb
+
+
+#設定於開機時自動Numlock
+echo  -e  "\n開始設定於開機時自動Numlock\n結束後請重新開機"
+sudo -i
+su gdm -s /bin/bash
+gsettings set org.gnome.settings-daemon.peripherals.keyboard numlock-state-'on'
 
 
 

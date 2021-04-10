d8520751de39aeaaa507999dda7e27ba360dea0d
rwxrwx777 <C109152304@nkust.edu.tw>
2021-04-10 13:53:39 +0800

    no message

---
+++ FlutterProjects/web_crawler_demo/android/app/src/main/kotlin/com/example/web_crawler_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.web_crawler_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ FlutterProjects/web_crawler_demo/lib/news.dart
@@ -0,0 +1,5 @@
+class News {
+  String date,message,department,link;
+
+  News(this.date,this.message,this.department,this.link);
+}

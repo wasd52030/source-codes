a1c5ea9363de1e188c384bba4f89520166076db3
rwxrwx777 <C109152304@nkust.edu.tw>
2021-04-14 23:40:58 +0800

    no message

--- 20210303_py_hw/main.py
+++ 20210303_py_hw/main.py
@@ -7,14 +7,12 @@ in_data=[]
 with open('input.txt') as f1:
     in_data=[k for k in f1]
 
-x=0
 out=""
 for i in range(1,int(in_data[0])+2):
     for k in range(int(in_data[0])+1,i+1,-1):
         out+=" "
     for j in range(1,i+1):
         out+=" {} ".format(math.comb(i-1,j-1))
-        x+=1
     out+="  "
     out+="\n"
 

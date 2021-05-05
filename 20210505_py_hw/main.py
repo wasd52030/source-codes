2e32eae237d9c64b2eb6b42b432b2105c3c7d961
rwxrwx777 <C109152304@nkust.edu.tw>
2021-05-06 01:45:11 +0800

    no message

--- 20210505_py_hw/main.py
+++ 20210505_py_hw/main.py
@@ -4,20 +4,20 @@ from tkinter import *
 import random
 
 ImageLst = []
-ImageFileNames=[]
-ItemWithIndex={}
+ImageFileNames = []
+ItemWithIndex = {}
 lbls = []
 n = 0
 speedCtl, lvlCtl = 0, 0
 speed = 0.01
 ans = 0
 stopflag = False
-lbla=None
+lbla = None
 root = Tk()
 
 
 def data_init():
-    global ans, speedCtl, lvlCtl, speed, stopflag,ItemWithIndex
+    global ans, speedCtl, lvlCtl, speed, stopflag, ItemWithIndex
     stopflag = False
     speed = 0.01
     ans = random.randint(0, 24)
@@ -25,7 +25,7 @@ def data_init():
 
 
 def main_anime():
-    global n, speed, speedCtl, ans, lvlCtl, stopflag,lbla
+    global n, speed, speedCtl, ans, lvlCtl, stopflag, lbla
 
     t1 = Timer(speed, main_anime)
     t1.start()
@@ -39,8 +39,7 @@ def main_anime():
         if ans == n:
             stopflag = True
             t1.cancel()
-            lbla['text']=ItemWithIndex[ans]
-            print(f'ans={ans}\nn={n}')
+            lbla['text'] = ItemWithIndex[ans]
 
     if stopflag == False:
         if 6 <= n < 16 and n % 2 == 0:
@@ -75,12 +74,12 @@ def run():
 
 
 def initial():
-    global ItemWithIndex,ImageFileNames,lbla
+    global ItemWithIndex, ImageFileNames, lbla
     # 取images資料夾下的所有圖片，匯集成tkinter.PhotoImage之list
-    x=0
+    x = 0
     for fname in os.listdir('images/'):
         file_dicrectory = os.path.join('images/', fname)
-        n=fname.split('.')
+        n = fname.split('.')
         ImageFileNames.append(n[0])
         ImageLst.append(PhotoImage(file=f'{file_dicrectory}'))
 
@@ -91,22 +90,21 @@ def initial():
                 a = Label(root, image=ImageLst[w], bg='#ffffff')
                 a.grid(row=i, column=j)
                 lbls.append(a)
-                ItemWithIndex[x]=ImageFileNames[w]
-                x+=1
-                
+                ItemWithIndex[x] = ImageFileNames[w]
+                x += 1
+
             elif j == 0 or j == 6:
                 a = Label(root, image=ImageLst[w], bg='#ffffff')
                 a.grid(row=i, column=j)
                 lbls.append(a)
-                ItemWithIndex[x]=ImageFileNames[w]
-                x+=1
-                
+                ItemWithIndex[x] = ImageFileNames[w]
+                x += 1
 
     b = Button(root, height=2, width=5,relief='groove', text='開始', command=run)
     b.grid(row=3, column=3)
-    lbla = Label(root,text='')
-    lbla.grid(row=4,column=1,columnspan=5)
-    print(ItemWithIndex)
+    lbla = Label(root, text='')
+    lbla.grid(row=4, column=1, columnspan=5)
+
 
 def main():
     initial()

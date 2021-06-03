db3c068513aa97b67ca9f81f3627fe3cab837388
rwxrwx777 <C109152304@nkust.edu.tw>
2021-06-03 08:04:04 +0800

    no message

--- PythonSQLDemo/WSQL.py
+++ PythonSQLDemo/WSQL.py
@@ -8,13 +8,16 @@ rd = res.json()['retVal']
 title = [f'`{x}`' for x in rd[0].keys()]
 d = [list(i.values()) for i in rd]
 
-#創資料表，在創建時同時檢查資料表是否已存在
+# 創資料表，在創建時同時檢查資料表是否已存在
 crtable = 'CREATE TABLE IF NOT EXISTS `n`.`ybk` ('
 crtable += ' VARCHAR(100) NOT NULL, '.join(title)
 crtable += ' VARCHAR(100) NOT NULL, PRIMARY KEY (`station_no`)) ENGINE = InnoDB;'
 
-#插入資料
-insert = "INSERT INTO `ybk` (`country_code`, `area_code`, `type`, `status`, `station_no`, `name_tw`, `district_tw`, `address_tw`, `name_en`, `district_en`, `address_en`, `name_cn`, `district_cn`, `address_cn`, `parking_spaces`, `available_spaces`, `empty_spaces`, `forbidden_spaces`, `lat`, `lng`, `img`, `updated_at`, `time`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+# 插入資料
+insert = "INSERT INTO `ybk` ("
+insert += ', '.join(title)
+insert += ') VALUES ('
+insert += '%s, '*(len(title)-1)+'%s)'
 
 try:
     conn = pymysql.connect(**sqlconfig.dbConfig)

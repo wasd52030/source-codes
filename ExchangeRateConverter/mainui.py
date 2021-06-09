32745d369ccc6ad2b925d57bf068cc8b2e02a63d
rwxrwx777 <C109152304@nkust.edu.tw>
2021-06-10 02:09:04 +0800

    no message

--- ExchangeRateConverter/mainui.py
+++ ExchangeRateConverter/mainui.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'designermRMIob.ui'
+# Form generated from reading UI file 'designermRMIob.ui'
 ##
-## Created by: Qt User Interface Compiler version 5.15.2
+# Created by: Qt User Interface Compiler version 5.15.2
 ##
-## WARNING! All changes made in this file will be lost when recompiling UI file!
+# WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
 from PySide2.QtCore import *

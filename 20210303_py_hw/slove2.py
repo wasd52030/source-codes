f54c2465d1519e044f8f1659843c970768b6447c
rwxrwx777 <C109152304@nkust.edu.tw>
2021-06-11 02:38:16 +0800

    no message

--- 20210303_py_hw/slove2.py
+++ 20210303_py_hw/slove2.py
@@ -7,19 +7,17 @@ with open('input.txt') as f1:
     in_data = [k for k in f1]
 
 d = [math.comb(i-1, j-1) for i in range(1, int(in_data[0])+2) for j in range(1, i+1)]
+
 cnt = 0
 f = True
 
 for i in range(1, int(in_data[0])+2):
     for j in range(1, (int(in_data[0])+2)-i+1):
-        Label(root).grid(row=i, column=j)
-    for k in range(1, 2*i-1):
-        r = k+j-1
+        n=j
+    for k in range(1, 2*i+1):
         f = not f
         if f == True:
-            Label(root, text=d[cnt]).grid(row=i, column=r)
+            Label(root, text=d[cnt]).grid(row=i, column=k+n)
             cnt += 1
-        else:
-            Label(root).grid(row=i, column=r+1)
 
 root.mainloop()

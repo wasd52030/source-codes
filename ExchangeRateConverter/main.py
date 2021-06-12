10bbd313fd1a2beb7e3271f3e124b5d6b955f519
rwxrwx777 <C109152304@nkust.edu.tw>
2021-06-12 22:09:12 +0800

    把介面重構

--- ExchangeRateConverter/main.py
+++ ExchangeRateConverter/main.py
@@ -22,45 +22,38 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = MainUi()
         self.ui.setupUi(self)
-        self.ui.btn1.clicked.connect(self.UpperToLower)
-        self.ui.btn2.clicked.connect(self.LowerToUpper)
+        self.ui.btn1.clicked.connect(self.switchbm)
+        self.ui.spinBox1.setMinimum(1)
         self.ui.spinBox1.setMaximum(100000000)
-        self.ui.spinBox2.setMaximum(100000000)
+        self.ui.spinBox1.valueChanged.connect(self.exchange)
+        self.ui.comboBox1.currentIndexChanged.connect(self.exchange)
+        self.ui.comboBox2.currentIndexChanged.connect(self.exchange)
         self.setcombo()
 
     def setcombo(self):
         self.ui.comboBox1.addItems(CurrencyNameLst)
         self.ui.comboBox2.addItems(CurrencyNameLst)
 
-    # 上面輸入框去換下面輸入框
-    def UpperToLower(self):
+    def switchbm(self):
+        comb1index = self.ui.comboBox1.currentIndex()
+        comb2index = self.ui.comboBox2.currentIndex()
+        self.ui.comboBox1.setCurrentIndex(comb2index)
+        self.ui.comboBox2.setCurrentIndex(comb1index)
+
+    def exchange(self):
         comb1value = self.ui.comboBox1.currentText()
         comb2value = self.ui.comboBox2.currentText()
-        num1value = self.ui.spinBox1.value()
-        num2value = self.ui.spinBox2.value()
+        r = self.ui.spinBox1.value()
+        if r == '':
+            self.ui.sipnBox1.setValue(1)
 
         if comb2value == 'USD':
-            r = num1value/CurrencyDict[f'USD{comb1value}']
-            self.ui.spinBox2.setValue(float(r))
+            r /= CurrencyDict[f'USD{comb1value}']
+            self.ui.lbl4.setText(str(r))
         else:
-            r = num1value/CurrencyDict[f'USD{comb1value}']
+            r /= CurrencyDict[f'USD{comb1value}']
             r *= CurrencyDict[f'USD{comb2value}']
-            self.ui.spinBox2.setValue(float(r))
-
-    # 下面輸入框去換上面輸入框
-    def LowerToUpper(self):
-        comb1value = self.ui.comboBox1.currentText()
-        comb2value = self.ui.comboBox2.currentText()
-        num1value = self.ui.spinBox1.value()
-        num2value = self.ui.spinBox2.value()
-
-        if comb1value == 'USD':
-            r = num2value/CurrencyDict[f'USD{comb2value}']
-            self.ui.spinBox1.setValue(float(r))
-        else:
-            r = num2value/CurrencyDict[f'USD{comb2value}']
-            r *= CurrencyDict[f'USD{comb1value}']
-            self.ui.spinBox1.setValue(float(r))
+            self.ui.lbl4.setText('{:.2f}'.format(r))
 
 
 app = QApplication(sys.argv)

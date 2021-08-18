d623174f43d9123996fe47cd2426fcdc1fe1ba93
rwxrwx777 <C109152304@nkust.edu.tw>
2021-08-19 02:23:18 +0800

    no message

---
+++ zerojudge_a915/main.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+
+struct point
+{
+	int x, y;
+};
+
+int main()
+{
+	int d = 0;
+	point ps[1000];
+	while (scanf("%d", &d) != EOF)
+	{
+		for (int i = 0; i < d; i++)
+		{
+			point a;
+			scanf("%d %d", &a.x, &a.y);
+			ps[i] = a;
+		}
+
+		//排x軸
+		for (int i = 0; i < d; i++)
+		{
+			for (int j = 0; j < d - 1 - i; j++)
+			{
+				if (ps[j].x > ps[j + 1].x)
+				{
+					std::swap(ps[j], ps[j + 1]);
+				}
+			}
+		}
+
+		//排y軸
+		for (int i = 0; i < d; i++)
+		{
+			for (int j = 0; j < d - 1 - i; j++)
+			{
+				if (ps[j].x == ps[j + 1].x)
+				{
+					if (ps[j].y > ps[j + 1].y)
+					{
+						std::swap(ps[j], ps[j + 1]);
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < d; i++)
+		{
+			printf("%d %d\n", ps[i].x, ps[i].y);
+		}
+	}
+
+	return 0;
+}

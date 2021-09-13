36af3b5d1b6a0bdb21885d6d9eade306dd500645
rwxrwx777 <C109152304@nkust.edu.tw>
2021-09-14 00:18:27 +0800

    no message

--- WhackMole.java
+++ WhackMole.java
@@ -3,116 +3,126 @@ import javax.swing.*;
 import java.util.*;
 import java.util.Timer;
 
-public class WhackMole
+public class WhackMole 
 {
-    JButton[] btns=new JButton[100];
-    JFrame mainWindow=new JFrame();
+    JButton[] btns = new JButton[100];
+    JFrame mainWindow = new JFrame();
     GridBagLayout gridBagLayout = new GridBagLayout();
-    int nums=(int)(Math.random()*11)+5;
+    int nums = (int) (Math.random() * 11) + 5;
     JLabel[] labels = new JLabel[3];
-    int score=0,level=1,cnt=0;
-    Timer timer=new Timer();
+    int score = 0, level = 1, cnt = 0;
+    Timer timer = new Timer();
 
-    WhackMole()
+    WhackMole() 
     {
         GameInit();
         SetLevelData();
-        timer.schedule(new TimerTask(){
+        timer.schedule(new TimerTask() 
+        {
             @Override
-            public void run() {
+            public void run() 
+            {
                 cnt++;
-                labels[0].setText(String.format("現在是第%d關，共%d分", level,score));
-                labels[1].setText(String.format("%d秒",cnt));
-                if (cnt==15)
+                labels[0].setText(String.format("現在是第%d關，共%d分", level, score));
+                labels[1].setText(String.format("%d秒", cnt));
+                if (cnt == 15) 
                 {
                     timer.cancel();
-                    JOptionPane.showMessageDialog(mainWindow, "遊戲結束", "",JOptionPane.WARNING_MESSAGE);
-                    for (JButton jButton : btns) jButton.setEnabled(false);
-                }
-                else if(cnt%5==0)
+                    JOptionPane.showMessageDialog(mainWindow, "遊戲結束", "", JOptionPane.WARNING_MESSAGE);
+                    for (JButton jButton : btns) 
+                    {
+                        jButton.setEnabled(false);
+                    }
+                } 
+                else if (cnt % 5 == 0) 
                 {
                     level++;
-                    nums=(int)(Math.random()*11)+5;
+                    nums = (int) (Math.random() * 11) + 5;
                     SetLevelData();
-                    labels[0].setText(String.format("現在是第%d關，共%d分", level,score));
-                    labels[2].setText(String.format("共%d隻地鼠",nums));
+                    labels[0].setText(String.format("現在是第%d關，共%d分", level, score));
+                    labels[2].setText(String.format("共%d隻地鼠", nums));
                 }
             }
-        },0,1000);
+        }, 0, 1000);
     }
 
-    void SetLevelData()
+    void SetLevelData() 
     {
-        int[] temp=new int[nums];
+        int[] temp = new int[nums];
 
-        for(var btn : btns) btn.setText("  ");
+        for (var btn : btns)
+            btn.setText("  ");
 
-        for (int i = 0; i < nums; i++)
+        for (int i = 0; i < nums; i++) 
         {
-            temp[i]=(int)(Math.random()*100);
+            temp[i] = (int) (Math.random() * 100);
             for (int j = 0; j < i;) 
             {
-                if (temp[j]==temp[i]) 
+                if (temp[j] == temp[i]) 
                 {
-                    temp[i]=(int)(Math.random()*100);
-                    j=0;
-                }
+                    temp[i] = (int) (Math.random() * 100);
+                    j = 0;
+                } 
                 else
                 {
                     j++;
                 }
             }
         }
-        for (int i : temp) btns[i].setText("X");
+
+        for (int i : temp) 
+        {
+            btns[i].setText("X");
+        }
     }
 
-    void GameInit()
+    void GameInit() 
     {
         mainWindow.setLayout(gridBagLayout);
-        int btncnt=0;
+        int btncnt = 0;
 
-        for (int i = 0; i < labels.length; i++)
+        for (int i = 0; i < labels.length; i++) 
         {
-            GridBagConstraints c=new GridBagConstraints();
-            c.fill=GridBagConstraints.BOTH;
-            c.gridx=0;
-            c.gridwidth=10;
-            c.gridy=i;
-            JLabel a=new JLabel("");
+            GridBagConstraints c = new GridBagConstraints();
+            c.fill = GridBagConstraints.BOTH;
+            c.gridx = 0;
+            c.gridwidth = 10;
+            c.gridy = i;
+            JLabel a = new JLabel("");
             a.setHorizontalAlignment(SwingConstants.CENTER);
-            labels[i]=a;
-            mainWindow.add(labels[i],c);
+            labels[i] = a;
+            mainWindow.add(labels[i], c);
         }
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 10; i++) 
         {
-            for (int j = 0; j < 10; j++)
+            for (int j = 0; j < 10; j++) 
             {
-                JButton btn=new JButton("");
-                btn.addActionListener(e->{
-                    if(btn.getText()=="X")
+                JButton btn = new JButton("");
+                btn.addActionListener(e -> {
+                    if (btn.getText() == "X") 
                     {
                         btn.setText("");
                         score++;
                     }
-                    labels[0].setText(String.format("現在是第%d關，共%d分", level,score));
+                    labels[0].setText(String.format("現在是第%d關，共%d分", level, score));
                 });
-                GridBagConstraints c=new GridBagConstraints();
-                c.fill=GridBagConstraints.BOTH;
-                c.gridx=i;
-                c.gridy=j+4;
-                mainWindow.add(btn,c);
-                btns[btncnt]=btn;
+                GridBagConstraints c = new GridBagConstraints();
+                c.fill = GridBagConstraints.BOTH;
+                c.gridx = i;
+                c.gridy = j + 4;
+                mainWindow.add(btn, c);
+                btns[btncnt] = btn;
                 btncnt++;
             }
         }
-        labels[2].setText(String.format("共%d隻地鼠",nums));
-        mainWindow.setSize(450,360);
+        labels[2].setText(String.format("共%d隻地鼠", nums));
+        mainWindow.setSize(450, 360);
     }
 
-    public static void main(String[] args)
+    public static void main(String[] args) 
     {
-        WhackMole a=new WhackMole();
+        WhackMole a = new WhackMole();
         a.mainWindow.setResizable(false);
         a.mainWindow.setVisible(true);
     }

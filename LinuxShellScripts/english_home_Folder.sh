01b5dc06f60339caae35123c276def7e6a778587
rwxrwx777 <C109152304@nkust.edu.tw>
2021-09-23 16:15:36 +0800

    no message

--- LinuxShellScripts/english_home_Folder.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo  "請在要改成英文資料夾的使用者下執行"
-echo  "在彈出的窗口中詢問是否将目錄轉化爲英文路徑,同意並關閉"
-
-export LANG=en_US
-xdg-user-dirs-gtk-update
-
--- LinuxShellScripts/remove gnome-initial-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#red hat系適用
-
-if [ $(id -u) -eq 0 ]; then
-	yum erase gnome-initial-setup
-else
-	echo "只有root權限之使用者才能執行"
-fi

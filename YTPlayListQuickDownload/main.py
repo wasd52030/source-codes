fa377cf5107c501ddfbfe8254b77073d782c28f4
rwxrwx777 <C109152304@nkust.edu.tw>
2021-12-29 14:27:31 +0800

    把當前下載狀態加回來

--- YTPlayListQuickDownload/main.py
+++ YTPlayListQuickDownload/main.py
@@ -7,6 +7,8 @@ from concurrent.futures import ThreadPoolExecutor
 
 
 def download(item):
+    global cnt, length
+
     try:
         item.streams.filter(only_audio=True).first().download()
         mp4name = item.streams.filter(only_audio=True).first().default_filename
@@ -24,14 +26,18 @@ def download(item):
         ], capture_output=True)
 
         os.remove(os.path.join(os.getcwd(), mp4name))  # 刪掉原本的mp4
-        print(f"{mp3name}\ndownload success！\n")
+        print(f"{mp3name}\ndownload success！")
+
+        cnt+=1
+        print(f"{cnt}/{length}\n")
     except:
         print("ERROR！")
 
 
-
 if __name__ == "__main__":
     p = Playlist(url=input("pls enter youtube playlist link："))
+    cnt = 0
+    length = p.length
 
     if not os.path.exists(f"./{p.title}"):
         os.mkdir(f"./{p.title}")
@@ -41,6 +47,6 @@ if __name__ == "__main__":
         print("floder created！")
         os.chdir(f"./{p.title}")
 
-    #max_workers請斟酌設定
+    # max_workers請斟酌設定
     with ThreadPoolExecutor(max_workers=8) as executer:
         executer.map(download, p.videos)

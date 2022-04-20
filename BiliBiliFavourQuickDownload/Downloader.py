af09a9f21a83e8a5e963c00e07be1a24d71ff0ce
wasd52030 <71220988+wasd52030@users.noreply.github.com>
2022-04-21 01:40:58 +0800

    修正了檔名中有雙引號引起的Bug 說實在的，有夠陰間

--- BiliBiliFavourQuickDownload/Downloader.py
+++ BiliBiliFavourQuickDownload/Downloader.py
@@ -1,3 +1,5 @@
+# Source: https://github.com/HFrost0/Lighting-bilibili-download
+
 import asyncio
 import anyio
 import httpx
@@ -5,6 +7,7 @@ import re
 import random
 import json
 import json5
+import html
 from datetime import datetime, timedelta
 import os
 from rich import print as rprint
@@ -15,7 +18,6 @@ from itertools import groupby
 class Downloader:
     def __init__(self, videos_dir='videos', sess_data='', video_concurrency=3, part_concurrency=10):
         """
-
         :param videos_dir: 下载到哪个目录，默认当前目录下的为videos中，如果路径不存在将自动创建
         :param sess_data: 有条件的用户填写大会员凭证，填写后可下载大会员资源
         :param video_concurrency: 限制最大同时下载的视频数量
@@ -25,8 +27,10 @@ class Downloader:
         if not os.path.exists(self.videos_dir):
             os.makedirs(videos_dir)
         cookies = {'SESSDATA': sess_data}
-        headers = {'user-agent': 'PostmanRuntime/7.29.0', 'referer': 'https://www.bilibili.com'}
-        self.client = httpx.AsyncClient(headers=headers, cookies=cookies, http2=True)
+        headers = {'user-agent': 'PostmanRuntime/7.29.0',
+                   'referer': 'https://www.bilibili.com'}
+        self.client = httpx.AsyncClient(
+            headers=headers, cookies=cookies, http2=True)
         self.progress = Progress(
             "{task.description}",
             "{task.percentage:>3.0f}%",
@@ -47,7 +51,8 @@ class Downloader:
         if 'cate_info' not in dir(self):
             self.cate_info = {}
             res = await self.client.get(
-                'https://s1.hdslb.com/bfs/static/laputa-channel/client/assets/index.c0ea30e6.js')
+                'https://s1.hdslb.com/bfs/static/laputa-channel/client/assets/index.c0ea30e6.js'
+            )
             cate_data = re.search('Za=([^;]*);', res.text).groups()[0]
             cate_data = json5.loads(cate_data)['channelList']
             for i in cate_data:
@@ -56,9 +61,19 @@ class Downloader:
                         self.cate_info[j['name']] = j
                 self.cate_info[i['name']] = i
 
-    async def get_favour(self, fid, num=20, keyword='', quality=0):
+    async def get_favour(self, fid, num=20, keyword='', quality=0, series=True):
+        """
+        下载收藏夹内的视频
+        :param fid: 收藏夹id
+        :param num: 下载数量
+        :param keyword: 搜索关键词
+        :param quality:
+        :param series: 每个视频是否下载所有p，False时仅下载系列中的第一个视频
+        :return:
+        """
         ps = 20
-        params = {'media_id': fid, 'pn': 1, 'ps': ps, 'keyword': keyword, 'order': 'mtime'}
+        params = {'media_id': fid, 'pn': 1, 'ps': ps,
+                  'keyword': keyword, 'order': 'mtime'}
         res = await self.client.get(url='https://api.bilibili.com/x/v3/fav/resource/list', params=params)
         data = json.loads(res.text)['data']
         # title = data['info']['title']
@@ -70,13 +85,25 @@ class Downloader:
                 num = total - (page_nums - 1) * ps
             else:
                 num = ps
-            cors.append(self.get_favor_by_page(fid, i + 1, num, keyword, quality))
+            cors.append(self.get_favor_by_page(
+                fid, i + 1, num, keyword, quality, series))
         await asyncio.gather(*cors)
 
-    async def get_favor_by_page(self, fid, pn=1, num=20, keyword='', quality=0):
+    async def get_favor_by_page(self, fid, pn=1, num=20, keyword='', quality=0, series=True):
+        """
+        下载收藏夹某一页的视频
+        :param fid: 收藏夹id
+        :param pn: 页号
+        :param num: 下载数量
+        :param keyword: 搜索关键词
+        :param quality:
+        :param series: 每个视频是否下载所有p，False时仅下载系列中的第一个视频
+        :return:
+        """
         ps = 20
         num = min(ps, num)
-        params = {'media_id': fid, 'order': 'mtime', 'ps': ps, 'pn': pn, 'keyword': keyword}
+        params = {'media_id': fid, 'order': 'mtime',
+                  'ps': ps, 'pn': pn, 'keyword': keyword}
         res = await self.client.get('https://api.bilibili.com/x/v3/fav/resource/list', params=params)
         res.raise_for_status()
         info = json.loads(res.text)
@@ -86,19 +113,20 @@ class Downloader:
             if i['title'] == '已失效视频':
                 rprint(f'[red]已失效视频 https://www.bilibili.com/video/{bvid}')
             else:
-                cors.append(self.get_series(f'https://www.bilibili.com/video/{bvid}', quality))
+                cors.append(self.get_series(f'https://www.bilibili.com/video/{bvid}', quality) if series else
+                            self.get_video(f'https://www.bilibili.com/video/{bvid}', quality))
         await asyncio.gather(*cors)
 
-    async def get_cate_videos(self, cate_name: str, num=10, order='click', keyword='', days=7, quality=0):
+    async def get_cate_videos(self, cate_name: str, num=10, order='click', keyword='', days=7, quality=0, series=True):
         """
         下载分区视频
-
         :param cate_name: 分区名称
         :param num: 下载数量
         :param order: 何种排序，click播放数，scores评论数，stow收藏数，coin硬币数，dm弹幕数
         :param keyword: 搜索关键词
-        :param quality: 画面质量
         :param days: 过去days天中的结果
+        :param quality: 画面质量
+        :param series: 每个视频是否下载所有p，False时仅下载系列中的第一个视频
         :return:
         """
         await self._load_cate_info()
@@ -112,38 +140,43 @@ class Downloader:
         cate_id = self.cate_info[cate_name]['tid']
         time_to = datetime.now()
         time_from = time_to - timedelta(days=days)
-        time_from, time_to = time_from.strftime('%Y%m%d'), time_to.strftime('%Y%m%d')
+        time_from, time_to = time_from.strftime(
+            '%Y%m%d'), time_to.strftime('%Y%m%d')
         pagesize = 30
         page = 1
         cors = []
         while num > 0:
             params = {'search_type': 'video', 'view_type': 'hot_rank', 'cate_id': cate_id, 'pagesize': pagesize,
                       'keyword': keyword, 'page': page, 'order': order, 'time_from': time_from, 'time_to': time_to}
-            cors.append(self._get_cate_videos_by_page(num=min(pagesize, num), params=params, quality=quality))
+            cors.append(self._get_cate_videos_by_page(
+                min(pagesize, num), params, quality, series))
             num -= pagesize
             page += 1
         await asyncio.gather(*cors)
 
-    async def _get_cate_videos_by_page(self, num, params, quality=0):
+    async def _get_cate_videos_by_page(self, num, params, quality=0, series=True):
         res = await self.client.get('https://s.search.bilibili.com/cate/search', params=params)
         info = json.loads(res.text)
         info = info['result'][:num]
-        cors = [self.get_series(f"https://www.bilibili.com/video/{i['bvid']}", quality=quality)
+        cors = [self.get_series(f"https://www.bilibili.com/video/{i['bvid']}", quality=quality) if series else
+                self.get_video(
+                    f"https://www.bilibili.com/video/{i['bvid']}", quality=quality)
                 for i in info]
         await asyncio.gather(*cors)
 
-    async def get_up_videos(self, mid: str, total=10, order='pubdate', keyword='', quality=0):
+    async def get_up_videos(self, mid: str, total=10, order='pubdate', keyword='', quality=0, series=True):
         """
-
         :param mid: b站用户id，在空间页面的url中可以找到
         :param total: 下载总数
         :param order: 何种排序，b站支持：最新发布pubdate，最多播放click，最多收藏stow
         :param keyword: 过滤关键词
         :param quality: 下载视频画面的质量，默认0为可下载的最高画质，数字越大质量越低，数值超过范围时默认选取最低画质
+        :param series: 每个视频是否下载所有p，False时仅下载系列中的第一个视频
         :return:
         """
         ps = 30
-        params = {'mid': mid, 'order': order, 'ps': ps, 'pn': 1, 'keyword': keyword}
+        params = {'mid': mid, 'order': order,
+                  'ps': ps, 'pn': 1, 'keyword': keyword}
         res = await self.client.get('https://api.bilibili.com/x/space/arc/search', params=params)
         res.raise_for_status()
         info = json.loads(res.text)
@@ -155,49 +188,54 @@ class Downloader:
                 num = total - (page_nums - 1) * ps
             else:
                 num = ps
-            cors.append(self.get_up_videos_by_page(mid, i + 1, num, order, keyword, quality))
+            cors.append(self.get_up_videos_by_page(
+                mid, i + 1, num, order, keyword, quality, series))
         await asyncio.gather(*cors)
 
-    async def get_up_videos_by_page(self, mid, pn=1, num=30, order='pubdate', keyword='', quality=0):
+    async def get_up_videos_by_page(self, mid, pn=1, num=30, order='pubdate', keyword='', quality=0, series=True):
         """
-
         :param mid: b站用户id，在空间页面的url中可以找到
         :param pn: 页码
         :param num: 下载数量
         :param order: 何种排序，b站支持：最新发布pubdate，最多播放click，最多收藏stow
         :param keyword: 过滤关键词
         :param quality: 下载视频画面的质量，默认0为可下载的最高画质，数字越大质量越低，数值超过范围时默认选取最低画质
+        :param series: 每个视频是否下载所有p，False时仅下载系列中的第一个视频
         :return:
         """
         ps = 30
         num = min(ps, num)
-        params = {'mid': mid, 'order': order, 'ps': ps, 'pn': pn, 'keyword': keyword}
+        params = {'mid': mid, 'order': order,
+                  'ps': ps, 'pn': pn, 'keyword': keyword}
         res = await self.client.get('https://api.bilibili.com/x/space/arc/search', params=params)
         res.raise_for_status()
         info = json.loads(res.text)
         bv_ids = [i['bvid'] for i in info['data']['list']['vlist']][:num]
         await asyncio.gather(
-            *[self.get_series(f'https://www.bilibili.com/video/{bv}', quality) for bv in bv_ids]
+            *[self.get_series(f'https://www.bilibili.com/video/{bv}', quality) if series else
+              self.get_video(f'https://www.bilibili.com/video/{bv}', quality)
+              for bv in bv_ids]
         )
 
     async def get_series(self, url: str, quality: int = 0):
         """
         下载某个系列（包括up发布的多p投稿，动画，电视剧，电影等）的所有视频。只有一个视频的情况下仍然可用该方法
-
         :param url: 系列中任意一个视频的url
         :param quality: 下载视频画面的质量，默认0为可下载的最高画质，数字越大质量越低，数值超过范围时默认选取最低画质
         :return:
         """
         url = url.split('?')[0]
         res = await self.client.get(url, follow_redirects=True)
-        initial_state = re.search(r'<script>window.__INITIAL_STATE__=({.*});\(', res.text).groups()[0]
+        initial_state = re.search(
+            r'<script>window.__INITIAL_STATE__=({.*});\(', res.text).groups()[0]
         initial_state = json.loads(initial_state)
         video_urls = []
         add_names = []
         if 'videoData' in initial_state:  # bv视频
             for idx, i in enumerate(initial_state['videoData']['pages']):
                 video_urls.append(f"{url}?p={idx + 1}")
-                add_names.append(i['part'] if len(initial_state['videoData']['pages']) > 1 else '')
+                add_names.append(i['part'] if len(
+                    initial_state['videoData']['pages']) > 1 else '')
         elif 'initEpList' in initial_state:  # 动漫，电视剧，电影
             for i in initial_state['initEpList']:
                 video_urls.append(i['link'])
@@ -212,7 +250,6 @@ class Downloader:
     async def get_video(self, url, quality: int = 0, add_name='', ):
         """
         下载单个视频
-
         :param url: 视频的url
         :param quality: 下载视频画面的质量，默认0为可下载的最高画质，数字越大质量越低，数值超过范围时默认选取最低画质
         :param add_name: 给文件的额外添加名，用户请直接保持默认
@@ -229,7 +266,14 @@ class Downloader:
         else:
             rprint(f'[red]超过重复次数 {url}')
             return
-        title = re.search('<h1[^>]*title="([^"]*)"', res.text).groups()[0].strip()
+        title = r'{}'.format(
+                html.unescape(
+                    re.search(
+                        '<h1[^>]*title="([^"]*)"',
+                        res.text
+                    ).groups()[0].strip()
+                )
+        )
         if add_name:
             title += f'-{add_name.strip()}'
         # replace windows illegal character in title
@@ -240,16 +284,19 @@ class Downloader:
             return
         # find video and audio url
         try:
-            play_info = re.search('<script>window.__playinfo__=({.*})</script><script>', res.text).groups()[0]
+            play_info = re.search(
+                '<script>window.__playinfo__=({.*})</script><script>', res.text).groups()[0]
             play_info = json.loads(play_info)
             # choose quality
             for q, (_, i) in enumerate(groupby(play_info['data']['dash']['video'], key=lambda x: x['id'])):
                 video_info = next(i)
-                video_urls = (video_info['base_url'], *(video_info['backup_url'] if video_info['backup_url'] else ()))
+                video_urls = (
+                    video_info['base_url'], *(video_info['backup_url'] if video_info['backup_url'] else ()))
                 if q == quality:
                     break
             audio_info = play_info['data']['dash']['audio'][0]
-            audio_urls = (audio_info['base_url'], *(audio_info['backup_url'] if audio_info['backup_url'] else ()))
+            audio_urls = (
+                audio_info['base_url'], *(audio_info['backup_url'] if audio_info['backup_url'] else ()))
         except (KeyError, AttributeError):  # KeyError-电影，AttributeError-动画
             # todo https://www.bilibili.com/video/BV1Jx411r776?p=3 未处理，老视频mp4
             rprint(f'[rgb(234,122,153)]{title} 需要大会员，或该地区不支持')
@@ -263,8 +310,14 @@ class Downloader:
         )
         self.sema.release()  # let next task begin
         await anyio.run_process(
-            ['ffmpeg', '-i', f'{self.videos_dir}/{title}-video.m4s', '-i', f'{self.videos_dir}/{title}-audio.m4s',
-             '-codec', 'copy', f'{self.videos_dir}/{title}.mp4', '-loglevel', 'quiet'])
+            [
+                'ffmpeg', '-i', f'{self.videos_dir}/{title}-video.m4s', '-i', f'{self.videos_dir}/{title}-audio.m4s',
+                '-codec', 'copy',
+                f'{self.videos_dir}/{title}.mp4',
+                '-loglevel',
+                'quiet'
+            ]
+        )
         os.remove(f'{self.videos_dir}/{title}-video.m4s')
         os.remove(f'{self.videos_dir}/{title}-audio.m4s')
         self.progress.update(task_id, advance=1)
@@ -274,16 +327,19 @@ class Downloader:
     async def _get_media(self, media_urls: tuple, media_name, task_id):
         res = await self.client.head(random.choice(media_urls))
         total = int(res.headers['Content-Length'])
-        self.progress.update(task_id, total=self.progress.tasks[task_id].total + total)
+        self.progress.update(
+            task_id, total=self.progress.tasks[task_id].total + total)
         part_length = total // self.part_concurrency
         cors = []
         part_names = []
         for i in range(self.part_concurrency):
             start = i * part_length
-            end = (i + 1) * part_length - 1 if i < self.part_concurrency - 1 else total - 1
+            end = (i + 1) * part_length - \
+                1 if i < self.part_concurrency - 1 else total - 1
             part_name = f'{media_name}-{start}-{end}'
             part_names.append(part_name)
-            cors.append(self._get_media_part(media_urls, (start, end), part_name, task_id))
+            cors.append(self._get_media_part(
+                media_urls, (start, end), part_name, task_id))
         await asyncio.gather(*cors)
         async with await anyio.open_file(f'{self.videos_dir}/{media_name}.m4s', 'wb') as f:
             for part_name in part_names:
@@ -318,11 +374,10 @@ if __name__ == '__main__':
         # await d.get_series(
         #     'https://www.bilibili.com/video/BV1Rx411B7oa?spm_id_from=333.999.0.0'
         #     , quality=0)
-        # await d.get_up_videos('18225678')
+        # await d.get_up_videos('18225678', total=5)
         # await d.get_cate_videos('宅舞', order='stow', days=30, keyword='超级敏感', num=100)
         # await d.get_video('https://www.bilibili.com/bangumi/play/ep471897?from_spmid=666.5.0.0')
-        await d.get_favour('840276009', num=10)
+        await d.get_favour('840297609', num=3, series=False)
         await d.aclose()
-
 
     asyncio.run(main())
--- BiliBiliFavourQuickDownload/main.py
+++ BiliBiliFavourQuickDownload/main.py
@@ -1,6 +1,7 @@
 # 請把ffmpeg.exe放進跟程式檔同一目錄下
 
 from urllib.parse import parse_qs, urlparse
+import traceback
 import html
 import asyncio
 import requests
@@ -12,22 +13,33 @@ from Downloader import Downloader
 
 async def download(id, num):
     d = Downloader(videos_dir=os.curdir, video_concurrency=5)
-    await d.get_favour(fid=id, quality=100, num=num, keyword='')
+    await d.get_favour(fid=id, quality=100, num=num, keyword='', series=False)
     await d.aclose()
 
 
 def ToMp3(video):
-    subprocess.run([
-        os.path.join(
-            os.path.abspath(os.path.join(os.getcwd(), os.path.pardir)),
-            "ffmpeg.exe"
-        ),
-        "-i",
-        os.path.join(os.getcwd(), video),
-        os.path.join(os.getcwd(), str(html.unescape(video)).replace("mp4", "mp3"))
-    ], capture_output=True)
+    global len, cnt
 
-    os.remove(os.path.join(os.getcwd(), video))  # 刪掉原本的mp4
+    try:
+        subprocess.run([
+            os.path.join(
+                os.path.abspath(os.path.join(os.getcwd(), os.path.pardir)),
+                "ffmpeg.exe"
+            ),
+            "-i",
+            os.path.join(os.getcwd(), video),
+            os.path.join(
+                os.getcwd(),
+                str(html.unescape(video)).replace("mp4", "mp3")
+            )
+        ], capture_output=True)
+
+        os.remove(os.path.join(os.getcwd(), video))  # 刪掉原本的mp4
+
+        cnt += 1
+        print(f"{html.unescape(video)} 轉換完成，目前進度: {cnt}/{len}")
+    except:
+        print(traceback.format_exc())
 
 
 if __name__ == "__main__":
@@ -40,7 +52,7 @@ if __name__ == "__main__":
         f"https://api.bilibili.com/x/v3/fav/resource/list?media_id={parameters['fid'][0]}&ps=1"
     ).json()['data']['info']
 
-    if not os.path.exists(f"./{FavourInfo['title']}"):
+    if not os.path.exists(f"./Bilibili-{FavourInfo['title']}"):
         os.mkdir(f"./Bilibili-{FavourInfo['title']}")
         os.chdir(f"./Bilibili-{FavourInfo['title']}")
         print("floder created！")
@@ -52,6 +64,7 @@ if __name__ == "__main__":
     print("mp4下載完成，將轉成mp3")
 
     mp4s = os.listdir('./')
+    cnt, len = 0, len(mp4s)
     with ThreadPoolExecutor(max_workers=20) as executer:
         executer.map(ToMp3, mp4s)
     print("全部mp3轉換完成")

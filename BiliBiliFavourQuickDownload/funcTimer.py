4de9dbc3f98f96bf8d3512afd0b292046fc9899b
wasd52030 <71220988+wasd52030@users.noreply.github.com>
2022-04-27 01:19:23 +0800

    把 datetime.timedelta 輸出的秒以下的數字清除

--- BiliBiliFavourQuickDownload/funcTimer.py
+++ BiliBiliFavourQuickDownload/funcTimer.py
@@ -1,4 +1,3 @@
-from textwrap import wrap
 import time
 from datetime import timedelta
 
@@ -8,6 +7,6 @@ def FuncTimer(func):
         t1 = time.time()
         func()
         t2 = time.time()
-        print(f"執行共花{timedelta(seconds=t2-t1)}")
+        print(f"執行共花 {str(timedelta(seconds=t2-t1))[0:-7]}")
 
     return wrap
--- YTPlayListQuickDownload/funcTimer.py
+++ YTPlayListQuickDownload/funcTimer.py
@@ -7,6 +7,6 @@ def FuncTimer(func):
         t1 = time.time()
         func()
         t2 = time.time()
-        print(f"執行共花 {timedelta(seconds=t2-t1)}")
+        print(f"執行共花 {str(timedelta(seconds=t2-t1))[0:-7]}")
 
     return wrap

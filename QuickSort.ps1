c415b5e75eb95fe705bce3532a9d8649fef11660
sobol <nppstcc1001050311@gmail.com>
2022-08-24 00:20:37 +0800

    no message

--- QuickSort.ps1
+++ QuickSort.ps1
@@ -3,29 +3,21 @@
 # 沒想到還真的能成功，酷欸
 # 不知道bash能不能那麼猛
 
-function QuickSort {
-    param (
-        [array]$arr
-    )
-
+function QuickSort([array]$arr) {
     if ($arr.Length -lt 2) {
         return $arr
     }
 
     $key = $arr[0]
     $arr = $arr[1..$arr.Length]
-    $left = $arr | Where-Object { $_ -lt $key } # Where-Object -> 類似其他語言的filter
+    $left = $arr | Where-Object { $_ -le $key } # Where-Object -> 類似其他語言的filter
     $right = $arr | Where-Object { $_ -gt $key } 
 
     return @(QuickSort($left)) + @($key) + @(QuickSort($right)) | Where-Object { $_ -ne $null }
 }
 
 
-function ArrayPrint {
-    param (
-        [array]$arr
-    )
-
+function ArrayPrint ([array]$arr) {
     for ($i = 0; $i -lt $arr.Length; $i++) {
         if ($i -ne $arr.Length - 1) {
             Write-Host ("{0} " -f $arr[$i]) -NoNewline # Write-Host 預設換行，不換行需加 -NoNewline argument
@@ -43,8 +35,7 @@ function main {
     ArrayPrint($a)
 
     Write-Host "After Sort"
-    $b = QuickSort($a)
-    ArrayPrint($b)
+    ArrayPrint(QuickSort($a))
 }
 
 main

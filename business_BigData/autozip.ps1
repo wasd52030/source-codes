0896233659776b061323f6e1a015dc12bdc38fbe
sobol <nppstcc1001050311@gmail.com>
2022-10-22 11:56:51 +0800

    排除markdown檔，不壓進壓縮檔內

--- business_BigData/autozip.ps1
+++ business_BigData/autozip.ps1
@@ -1,16 +1,19 @@
 function main {
+
+    $excludes = @("*.md")
+
     if (!(Test-Path './Compressed_for_HW')) {
         mkdir './Compressed_for_HW'
     }
 
     #這裡會以能不能轉int為filter的條件是因為每週作業的資料夾皆以當天日期(YYYYMMDD)為名，剛好可以濾掉存壓縮檔的資料夾Compressed_for_HW
-    $a = Get-ChildItem -Directory | Where-Object { $_.BaseName -as [int] }
+    $a = Get-ChildItem -Directory -exclude "*md" | Where-Object { $_.BaseName -as [int] }
     for ($i = 0; $i -lt $a.Count; $i++) {
         if ($i -lt 10) {
-            Compress-Archive -Path "$($a[$i].BaseName)/*" -Force "./Compressed_for_HW/hw0$($i+1).zip"
+            Get-ChildItem $a[$i].BaseName -Exclude $excludes | Compress-Archive -Force -DestinationPath "./Compressed_for_HW/hw0$($i+1).zip"
         }
         else {
-            Compress-Archive -Path "$($a[$i].BaseName)/*" -Force "./Compressed_for_HW/hw$($i+1).zip" 
+            Get-ChildItem $a[$i].BaseName -Exclude $excludes | Compress-Archive -Force -DestinationPath "./Compressed_for_HW/hw$($i+1).zip" 
         }
     }
 

49c9fed065e68a750df089eaeeb6eca2394f6fa2
sobol <nppstcc1001050311@gmail.com>
2022-12-20 00:26:01 +0800

    重構以符合教授命名規則(這個人學期末才改......Haiyaa~)

--- business_BigData/autozip.ps1
+++ business_BigData/autozip.ps1
@@ -6,15 +6,17 @@ function main {
         mkdir './Compressed_for_HW'
     }
 
-    # 這裡會以能不能轉int為filter的條件是因為每週作業的資料夾皆以教授出作業的當天日期(YYYYMMDD)為名
-    # 剛好可以濾掉存壓縮檔的資料夾Compressed_for_HW
-    $a = Get-ChildItem -Directory | Where-Object { $_.BaseName -as [int] }
+    $a = Get-ChildItem -Directory | Where-Object { $_.BaseName -ne "Compressed_for_HW" }
     for ($i = 0; $i -lt $a.Count; $i++) {
-        if ($i -lt 10) {
-            Get-ChildItem $a[$i].BaseName -Exclude $excludes | Compress-Archive -Force -DestinationPath "./Compressed_for_HW/hw0$($i+1).zip"
-        }
-        else {
-            Get-ChildItem $a[$i].BaseName -Exclude $excludes | Compress-Archive -Force -DestinationPath "./Compressed_for_HW/hw$($i+1).zip" 
+        if ($a[$i].BaseName -as [int]) {
+            if ($i -lt 10) {
+                Get-ChildItem $a[$i].BaseName -Exclude $excludes | Compress-Archive -Force -DestinationPath "./Compressed_for_HW/hw0$($i+1).zip"
+            }
+            else {
+                Get-ChildItem $a[$i].BaseName -Exclude $excludes | Compress-Archive -Force -DestinationPath "./Compressed_for_HW/hw$($i+1).zip" 
+            }
+        }else {
+            Get-ChildItem $a[$i].BaseName -Exclude $excludes | Compress-Archive -Force -DestinationPath "./Compressed_for_HW/$($a[$i].BaseName).zip" 
         }
     }
 

2da0983270b8549e29302e4c9b2afe71a579c2b5
sobol <nppstcc1001050311@gmail.com>
2022-12-30 20:10:40 +0800

    修正一點bug、以generator和enumerate重寫遊戲主邏輯

--- 20201216_py_hw/main.py
+++ 20201216_py_hw/main.py
@@ -1,34 +1,27 @@
 import random
+import os
+
 data = []
 money = 100
-yeslocate = False
-yesvalue = False
-justyesvalue = 0
 wincounter = 0
-doubleyes = 0
 user_input = []
 kin_flag = 0
 rein = 0
 
 
 def game_input():
-    global user_input, money, doubleyes, justyesvalue, rein, wincounter, data
+    global user_input, money, rein, wincounter, data
     user_input.clear()
     if rein != 1:
         money -= 10
     rein = 0
-    doubleyes = 0
-    justyesvalue = 0
     print("你有%d塊" % money)
     print("輸入一四位數的數字(不得重複):", end="")
-    a = input()
-    for k in range(0, len(a)):
-        user_input.append(int(a[k]))
-    for a in range(0, len(user_input)-1):
-        if user_input[a] == user_input[a+1]:
-            print("輸入的數字有重複，請重新輸入")
-            rein = 1
-            game_input()
+    user_input = list(map(int, input()))
+    if len(set(user_input)) != len(user_input):
+        print("輸入的數字有重複，請重新輸入")
+        rein = 1
+        game_input()
     rein = 0
     wincounter += 1
     print("題目 : ", end="")
@@ -47,28 +40,22 @@ def calc():
 
 
 def run_game():
-    global data, user_input, money, yesvalue, yeslocate, doubleyes, justyesvalue, wincounter, rein
+    global data, user_input, money, wincounter, rein
+
     rein = 0
-    for i in range(0, len(user_input)):
-        for x in data:
-            if user_input[i] == x:
-                yesvalue = True
-                break
+    allclear, only_value = 0, 0
+
+    u = ([item, index] for index, item in enumerate(data))
+    v = ([item, index] for index, item in enumerate(user_input))
+    for du, dv in zip(u, v):
+        if dv[0] in data:
+            if dv[0] == du[0] and dv[1] == du[1]:
+                allclear += 1
             else:
-                yesvalue = False
+                only_value += 1
 
-        if user_input[i] == data[i]:
-            yeslocate = True
-        else:
-            yeslocate = False
-
-        if yeslocate == True and yeslocate == True:
-            doubleyes += 1
-        if yeslocate != True and yesvalue == True:
-            justyesvalue += 1
-
-    print("皆對%d個，位置錯%d個,猜了%d次" % (doubleyes, justyesvalue, wincounter))
-    if doubleyes == 4:
+    print("皆對%d個，位置錯%d個,猜了%d次" % (allclear, only_value, wincounter))
+    if allclear == 4:
         print("win")
         calc()
         main()
@@ -79,16 +66,19 @@ def run_game():
 
 def main():
     global data
-    while True:
-        print("現有金額 : %d" % money)
-        print("輸入1進遊戲，輸入0離開:", end="")
-        kin_flag = int(input())
-        if kin_flag == 1:
-            data = random.sample(list(range(0,10)), 4)
-            print(data)
-            game_input()
-        elif kin_flag == 0:
-            break
+
+    print("現有金額 : %d" % money)
+    print("輸入1進遊戲，輸入0離開:", end="")
+    kin_flag = int(input())
+    if kin_flag == 1:
+        data = random.sample(list(range(0, 10)), 4)
+        print(data)
+        game_input()
+    elif kin_flag==0:
+        os._exit(0)
+    else:
+        print("不支援指令")
+        main()
 
 
 main()

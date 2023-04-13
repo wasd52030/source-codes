bc33d0957b0c503f619eff2618d068c64dc469e9
sobol <nppstcc1001050311@gmail.com>
2023-04-13 15:11:39 +0800

    no message

---
+++ dllmer/Extensions.cs
@@ -0,0 +1,6 @@
+namespace dllmer;
+
+public static class Extensions
+{
+	public static string dllm(this String aaa) => $"屌你老母 {aaa}";
+}
--- dllmer/Form1.cs
+++ dllmer/Form1.cs
@@ -7,12 +7,13 @@ public partial class Form1 : Form
         InitializeComponent();
     }
 
-    private async void btn1_click(object sender, EventArgs e){
+    private async void btn1_click(object sender, EventArgs e)
+    {
         Console.WriteLine(label1.Size);
         for (int i = 0; i <= 100; i++)
         {
-            label1.Text=$"屌你老母 {i}";
-            progressBar1.Value=i;
+            label1.Text = i.ToString().dllm();
+            progressBar1.Value = i;
             await Task.Delay(100);
         }
         Console.WriteLine(label1.Size);

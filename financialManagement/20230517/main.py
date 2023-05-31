9350d33ccff597d73ca0345143a61b460a08c51e
sobol <nppstcc1001050311@gmail.com>
2023-05-31 09:33:27 +0800

    no message

--- financialManagement/20230517/main.py
+++ financialManagement/20230517/main.py
@@ -3,14 +3,14 @@ import pandas
 
 
 def discountedPaybackPeriod(
-    initial_investment: float, cash_flows: List[float], discount_rate: float
+    initialInvestment: float, cashFlows: List[float], discountRate: float
 ):
     t = [
-        [0, initial_investment, 1, initial_investment, initial_investment],
+        [0, initialInvestment, 1, initialInvestment, initialInvestment],
     ]
 
-    for i, cash_flow in enumerate(cash_flows):
-        p = 1 / (1 + discount_rate) ** (i + 1)
+    for i, cash_flow in enumerate(cashFlows):
+        p = 1 / (1 + discountRate) ** (i + 1)
         cp = round(cash_flow * p, 0)
         fp = abs(cp - t[i][-1])
         t.append(
@@ -18,14 +18,14 @@ def discountedPaybackPeriod(
         )
 
     t.append([None] * 5)
-    t.append(["折現回收期間", len(cash_flows) - 1 + t[-3][-1] / t[-2][-2]])
+    t.append(["折現回收期間", len(cashFlows) - 1 + t[-3][-1] / t[-2][-2]])
 
     table = pandas.DataFrame(
         t,
         columns=[
             "年度",
             "每年現金流入量",
-            f"折現因子(折現率={int(discount_rate*100)}%)",
+            f"折現因子(折現率={int(discountRate*100)}%)",
             "現金流量折現值",
             "累積現金流量折現值",
         ],
@@ -38,14 +38,14 @@ def discountedPaybackPeriod(
 
 
 def modifiedInternalRateOfReturn(
-    initial_investment: float, cash_flows: List[float], discount_rate: float
+    initial_investment: float, cash_flows: List[float], capitalRate: float
 ):
     # 期數
     n = len(cash_flows)
 
     sCIF = sum(
         [
-            cash * ((1 + discount_rate) ** (n - index - 1))
+            cash * ((1 + capitalRate) ** (n - index - 1))
             for index, cash in enumerate(cash_flows)
         ]
     )
@@ -60,17 +60,20 @@ def main():
     n = int(input("請輸入期數: "))
 
     # 輸入現金流
-    cash_flows = [float(input(f"請輸入第{i+1}期金額: ")) for i in range(n)]
+    cashFlows = [float(input(f"請輸入第{i+1}期金額: ")) for i in range(n)]
 
     # 折現率
-    discount_rate = float(input("請輸入折現率: "))
+    discountRate = float(input("請輸入折現率: "))
+
+    # 資金成本率
+    capitalRate = float(input("請輸入資金成本率: "))
 
     # 初始投資
-    initial_investment = int(input("請輸入初始投資額: "))
+    initialInvestment = int(input("請輸入初始投資額: "))
 
-    discountedPaybackPeriod(initial_investment, cash_flows, discount_rate)
+    discountedPaybackPeriod(initialInvestment, cashFlows, discountRate)
     print()
-    modifiedInternalRateOfReturn(initial_investment, cash_flows, discount_rate)
+    modifiedInternalRateOfReturn(initialInvestment, cashFlows, capitalRate)
 
 
 if __name__ == "__main__":

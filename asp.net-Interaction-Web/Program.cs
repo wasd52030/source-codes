3c97156ec9abbedfecd289a2d53e0d8698efab0b
sobol <C109152304@nkust.edu.tw>
2023-10-25 01:43:26 +0800

    update Program.cs

--- asp.net-Interaction-Web/Program.cs
+++ asp.net-Interaction-Web/Program.cs
@@ -1,8 +1,27 @@
+using _20231016.Data;
+using _20231016.Logger;
 using _20231016.Middlewares;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddLogging(logging=>{
+    logging.ClearProviders();
+    logging.AddApplicationLogger();
+});
+
+builder.Services.AddTransient<IEmployeeService, EmployeeService>();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(5,7,43))
+    );
+});
 
 var app = builder.Build();
 

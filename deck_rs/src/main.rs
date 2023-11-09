d94475888a0482e95343571629060876cf6c3ea3
sobol <C109152304@nkust.edu.tw>
2023-11-09 19:39:23 +0800

    practice for rust extension method

--- deck_rs/src/main.rs
+++ deck_rs/src/main.rs
@@ -1,11 +1,22 @@
-pub mod cardComponent;
 pub mod card;
+pub mod cardComponent;
 pub mod deck;
 
 use deck::Deck;
 
+pub trait dllmExt {
+    fn dllm(&self) -> String;
+}
 
-fn deckTest(){
+impl dllmExt for &str {
+    fn dllm(&self) -> String {
+        let u = "dllm".to_owned();
+        let v = self.clone();
+        return u.clone() + " " + v;
+    }
+}
+
+fn deckTest() {
     let mut d = Deck::new();
     d.shuffle();
     for i in 0..d.deckSize() {
@@ -18,5 +29,7 @@ fn deckTest(){
 }
 
 fn main() {
+    let u = "6";
+    println!("{}", u.dllm());
     deckTest();
 }

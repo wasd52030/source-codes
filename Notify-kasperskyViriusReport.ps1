93cf5f732dba757b49714da79b92d8dbcbe99187
sobel <nppstcc1001050311@gmail.com>
2025-01-26 20:03:37 +0800

    add Notify-kasperskyViriusReport

---
+++ Notify-kasperskyViriusReport.ps1
@@ -0,0 +1,22 @@
+﻿# 1555、2355、0755 kaspersky Virius Report Export to PDF
+# 2355 send all report pdf file that day to control officer
+
+[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
+$Template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText04)
+
+$RawXml = [xml] $Template.GetXml()
+# ($RawXml.toast.visual.binding.text | Where-Object { $_.id -eq "1" }).AppendChild($RawXml.CreateTextNode("")) > $null
+($RawXml.toast.visual.binding.text | Where-Object { $_.id -eq "2" }).AppendChild($RawXml.CreateTextNode("請值班同仁於1555(A)、2355(B)、0755(C)將卡巴病毒報告匯出成PDF")) > $null
+($RawXml.toast.visual.binding.text | Where-Object { $_.id -eq "3" }).AppendChild($RawXml.CreateTextNode("B班同仁匯出完後將當天全部的PDF檔案寄outlook給管制官")) > $null
+
+$SerializedXml = New-Object Windows.Data.Xml.Dom.XmlDocument
+$SerializedXml.LoadXml($RawXml.OuterXml)
+
+$Toast = [Windows.UI.Notifications.ToastNotification]::new($SerializedXml)
+
+$Toast.Tag = "PowerShell"
+$Toast.Group = "PowerShell"
+$Toast.ExpirationTime = [DateTimeOffset]::Now.AddMinutes(1)
+
+$Notifier = [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime]::CreateToastNotifier("kaspersky Virius Report！！！")
+$Notifier.Show($Toast);

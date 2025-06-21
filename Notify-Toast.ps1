f4bfa9e3f7ffa74497d53e60a9fad7abcb21407a
sobel <nppstcc1001050311@gmail.com>
2025-06-21 19:14:02 +0800

    no message

--- Notify-Toast.ps1
+++ Notify-Toast.ps1
@@ -1,13 +1,13 @@
-﻿# 1555、2355、0755 kaspersky Virius Report Export to PDF
-# 2355 send all report pdf file that day to control officer
+﻿# 利用 $PSVersionTable 檢查
+# 版本限制 -> PSVersion>=5 && PSVersion<7
 
 [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
 $Template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText04)
 
 $RawXml = [xml] $Template.GetXml()
 # ($RawXml.toast.visual.binding.text | Where-Object { $_.id -eq "1" }).AppendChild($RawXml.CreateTextNode("")) > $null
-($RawXml.toast.visual.binding.text | Where-Object { $_.id -eq "2" }).AppendChild($RawXml.CreateTextNode("請值班同仁於1555(A)、2355(B)、0755(C)將卡巴病毒報告匯出成PDF")) > $null
-($RawXml.toast.visual.binding.text | Where-Object { $_.id -eq "3" }).AppendChild($RawXml.CreateTextNode("B班同仁匯出完後將當天全部的PDF檔案寄outlook給領班")) > $null
+($RawXml.toast.visual.binding.text | Where-Object { $_.id -eq "2" }).AppendChild($RawXml.CreateTextNode("なんで春日影やったの")) > $null
+($RawXml.toast.visual.binding.text | Where-Object { $_.id -eq "3" }).AppendChild($RawXml.CreateTextNode("いいご身分ですわね")) > $null
 
 $SerializedXml = New-Object Windows.Data.Xml.Dom.XmlDocument
 $SerializedXml.LoadXml($RawXml.OuterXml)
@@ -18,5 +18,5 @@ $Toast.Tag = "PowerShell"
 $Toast.Group = "PowerShell"
 $Toast.ExpirationTime = [DateTimeOffset]::Now.AddMinutes(1)
 
-$Notifier = [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime]::CreateToastNotifier("kaspersky Virius Report！！！")
+$Notifier = [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime]::CreateToastNotifier("crychic")
 $Notifier.Show($Toast);
